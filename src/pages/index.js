eeb79c21fd511996d39bfc6222ad5bef38d0fda9
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-02-22 00:22:14 -0500

    made react component

---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { useStaticQuery, graphql, Link as GatsbyLink } from "gatsby";
+
+const HomePage = () =>  {
+
+    const data = useStaticQuery(`
+    query MyQuery {
+        allWpPost {
+          edges {
+            node {
+              id
+              content
+              uri
+            }
+          }
+        }
+      }`)
+
+      const { allWpPost } = data;
+
+      return (
+        <main>
+          <h1>Welcome to my Gatsby site!</h1>
+          <p>I'm making this by following the Gatsby Tutorial.</p>
+        </main>
+      )
+    }
+    
+    // You'll learn about this in the next task, just copy it for now
+    export const Head = () => <title>Home Page</title>
+    
+    // Step 3: Export your component
+    export default HomePage;
