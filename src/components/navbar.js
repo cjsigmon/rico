abc362d240d5d35c06de6bf6f27d73efb196dc51
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-03-01 16:01:08 -0500

    nav disappears on scroll down, reappears on up

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import '../styles.css';
 
-function hideNav() {
+const hideNav = () => {
     var prevScrollpos = window.pageYOffset;
     window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
@@ -13,20 +13,37 @@ function hideNav() {
   }
   prevScrollpos = currentScrollPos;
 }
-}
+};
 
 const Navbar = () => {
 
+  const data = useStaticQuery(graphql`
+  {
+    allWpPost {
+      nodes {
+        title
+        uri
+      }
+    }
+  }
+  `)
+
+      const { allWpPost } = data;
+
     return (
         <main>
-            <div onScroll="hideNav()" class="navbar">
-              <a href="/"><img src="https://picsum.photos/100/70"></img></a>
-              <h4>Environment</h4>
-              <h4>Community</h4>
-              <h4>Governance</h4>
-              <h4>Power</h4>
-              <h4>Healthcare</h4>
-              <h4>ABOUT</h4>
+          
+            <div onScroll={hideNav} class="navbar">
+              <a href="/"><img src="https://picsum.photos/100/80"></img></a>
+              { allWpPost.nodes.map( post => (
+                <a key={post.title} href={post.uri}><h4>{post.title}</h4></a>
+            ))}
+              {/* <a href="#env"><h4>Environment</h4></a>
+              <a href="#env"><h4>Community</h4></a>
+              <a href="#env"><h4>Governance</h4></a>
+              <a href="#env"><h4>Power</h4></a>
+              <a href="#env"><h4>Healthcare</h4></a>
+              <a href="#env"><h4>ABOUT</h4></a> */}
 
             </div>
         </main>
