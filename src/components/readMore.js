75ce092c2202cb0e31d8bc35a15fcc14eff052f9
Caleb Sigmon <cjsigmon@email.unc.edu>
2023-04-19 16:09:07 -0400

    added text to readmores

--- src/components/readMore.js
+++ src/components/readMore.js
@@ -28,11 +28,11 @@ export default function ReadMore({ exclude }) {
             <h2>MORE FROM ISLA DE FUERZA</h2>
             
             <div className="more-thumbs">
-                {exclude === 'Paso a paso' ? <></> :  <a href={powLink}><img src={powImage}></img></a>}
-                {exclude === 'COMMUNITY' ? <></> :  <a href={comLink}><img src={comImage}></img></a>}
-                {exclude === 'GOVERNANCE' ? <></> :  <a href={govLink}><img src={govImage}></img></a>}
-                {exclude === 'ENVIRONMENT' ? <></> :  <a href={envLink}><img src={envImage}></img></a>}
-                {exclude === 'HEALTHCARE' ? <></> :  <a href={healthLink}><img src={healthImage}></img></a>}
+                {exclude === 'Paso a paso' ? <></> :  <a href={powLink}><div className="readmore-hold"><img src={powImage}></img><h4 id="powread">Paso a paso</h4></div></a>}
+                {exclude === 'COMMUNITY' ? <></> :  <a href={comLink}><div className="readmore-hold"><img src={comImage}></img><h4 id="comread">COMMUNITY</h4></div></a>}
+                {exclude === 'GOVERNANCE' ? <></> :  <a href={govLink}><div className="readmore-hold"><img src={govImage}></img><h4 id="govread">GOVERNANCE</h4></div></a>}
+                {exclude === 'ENVIRONMENT' ? <></> :  <a href={envLink}><div className="readmore-hold"><img src={envImage}></img><h4 id="envread">ENVIRONMENT</h4></div></a>}
+                {exclude === 'HEALTHCARE' ? <></> :  <a href={healthLink}><div className="readmore-hold"><img src={healthImage}></img><h4 id="hearead">HEALTHCARE</h4></div></a>}
             </div>
         </div>
         
